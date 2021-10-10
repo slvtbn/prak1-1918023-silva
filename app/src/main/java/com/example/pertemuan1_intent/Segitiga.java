9eba4dd697909c785d4ebdb6b3c2c77d109de66e
SILVA TAMBUNAN <silvaanjelina07@gmail.com>
2021-10-10 21:23:37 +0900

    first commit

---
+++ app/src/main/java/com/example/pertemuan1_intent/Segitiga.java
@@ -0,0 +1,14 @@
+package com.example.pertemuan1_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Segitiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segitiga);
+    }
+}
